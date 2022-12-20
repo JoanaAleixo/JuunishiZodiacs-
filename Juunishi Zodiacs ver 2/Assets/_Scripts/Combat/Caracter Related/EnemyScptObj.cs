52175547c0e5b6366f15e05184676390ba066e1d
juunishi-zodiacs-admin <joana_aleixo@eticalgarve.com>
2022-12-20 16:41:04 +0000

    Apagar tudo

--- Juunishi Zodiacs ver 2/Assets/_Scripts/Combat/Caracter Related/EnemyScptObj.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "ScriptableObjects/Enemy")]
-public class EnemyScptObj : CaracterCreation
-{
-
-}
--- Juunishi Zodiacs ver 2/Assets/_Scripts/Combat/Caracter Related/PlayableCaracterScptObj.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "ScriptableObjects/PlayableCaracter")]
-public class PlayableCaracterScptObj : CaracterCreation
-{
-    [SerializeField] FloatVariable _sP;
-}
