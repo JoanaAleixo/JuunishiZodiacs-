ca77f70384356eee8ca5f0670e9df256aaf2cf01
KingsXz <leandro_reis@eticalgarve.com>
2023-05-19 22:17:16 +0100

    Inicio do SoundManager

--- Juunishi Zodiacs v2/Assets/_Scripts/Audio/SoundManager.cs
+++ Juunishi Zodiacs v2/Assets/_Scripts/Audio/SoundManager.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    [SerializeField] public static AudioClip slashSound, jumpSound, bomSound, starSound, posionSound, bigStarSound, spikesSound, foot1Sound, foot2Sound, waterSound;
-    static AudioSource audioSrc;
+    [SerializeField] AudioSource audioSourcePre;
 
     void Start()
     {
-        slashSound = Resources.Load<AudioClip>("Slash");
 
-        audioSrc = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,8 +17,9 @@ public class SoundManager : MonoBehaviour
 
     }
 
-    public static void PlaySound(AudioClip clip)
+    public void PlaySound(AudioClip clip)
     {
-        audioSrc.PlayOneShot(clip);
+        AudioSource insta = Instantiate(audioSourcePre);
+        insta.PlayOneShot(clip);
     }
 }
